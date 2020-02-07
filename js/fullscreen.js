72881ae2feb8beb603b7733ab889d26dffffea9b
Augustine <ashramwen@gmail.com>
2020-02-07 08:49:50 +0800

    refactor: 💡 Use single quote

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,39 +1,39 @@
 (function() {
-  if (!$("#content").has(".contextual").length) {
-    $("#content").prepend('<div class="contextual"></div>');
+  if (!$('#content').has('.contextual').length) {
+    $('#content').prepend('<div class="contextual"></div>');
   }
 
-  $(".contextual").prepend(
+  $('.contextual').prepend(
     '<a class="icon icon-fullscreen" href="javascript:void(0)">Full screen</a>'
     // '<a href="#" onclick="$(&quot;html&quot;).toggleClass(&quot;agile-board-fullscreen&quot;); saveFullScreenState(); return false;" class="icon icon-fullscreen">Full screen</a>'
   );
 
-  let fullscreen = !!JSON.parse(sessionStorage.getItem("fullscreen"));
+  let fullscreen = !!JSON.parse(sessionStorage.getItem('fullscreen'));
 
-  $(".icon.icon-fullscreen").click(() => {
+  $('.icon.icon-fullscreen').click(() => {
     fullscreen = !fullscreen;
-    sessionStorage.setItem("fullscreen", fullscreen);
+    sessionStorage.setItem('fullscreen', fullscreen);
 
     toggleFullscreen();
   });
 
   const toggleFullscreen = () => {
     // top-menu toggle
-    $("#top-menu").toggle(!fullscreen);
+    $('#top-menu').toggle(!fullscreen);
 
     // header toggle
-    $("#header > a").toggle(!fullscreen);
-    $("#header > #quick-search").toggle(!fullscreen);
-    $("#header > h1").toggle(!fullscreen);
+    $('#header > a').toggle(!fullscreen);
+    $('#header > #quick-search').toggle(!fullscreen);
+    $('#header > h1').toggle(!fullscreen);
 
     // sidebar toggle
-    $("#sidebar").toggle(!fullscreen);
+    $('#sidebar').toggle(!fullscreen);
     if (fullscreen) {
-      $("#content").width("auto");
-      $("#header").css("min-height", "auto");
+      $('#content').width('auto');
+      $('#header').css('min-height', 'auto');
     } else {
-      $("#content").width("75%");
-      $("#header").css("min-height", "5.3em");
+      $('#content').width('75%');
+      $('#header').css('min-height', '5.3em');
     }
   };
 
